fd3d72dd8a9fac8d9211c4e06199fb3c79acf153
William Woodall <william@osrfoundation.org>
2013-08-16 15:58:59 -0700

    improve test coverage

--- test/test_discovery.py
+++ test/test_discovery.py
@@ -13,6 +13,7 @@ test_data_dir = os.path.join(os.path.dirname(__file__), 'discovery_workspaces')
 
 
 def test_load_minimal():
+    print('Testing minimal workspace')
     workspaces = [os.path.join(test_data_dir, 'minimal')]
     package_index = package_index_from_package_path(workspaces)
     spec_file_index = spec_file_index_from_package_index(package_index)
@@ -29,14 +30,23 @@ def test_load_minimal():
 
 
 def test_load_invalid_specs():
+    print('Testing invalid_specs workspace')
     workspaces = [os.path.join(test_data_dir, 'invalid_specs')]
     package_index = package_index_from_package_path(workspaces)
     spec_file_index = spec_file_index_from_package_index(package_index)
     spec_index, errors = spec_index_from_spec_file_index(spec_file_index)
-    assert discovery.InterfaceNameNotFoundException in [type(x) for x in errors]
+
+
+def test_load_missing_interface():
+    print('Testing missing_interface workspace')
+    workspaces = [os.path.join(test_data_dir, 'missing_interface')]
+    package_index = package_index_from_package_path(workspaces)
+    spec_file_index = spec_file_index_from_package_index(package_index)
+    spec_index, errors = spec_index_from_spec_file_index(spec_file_index)
 
 
 def test_load_duplicate_names():
+    print('Testing duplicate_names workspace')
     workspaces = [os.path.join(test_data_dir, 'duplicate_names')]
     package_index = package_index_from_package_path(workspaces)
     spec_file_index = spec_file_index_from_package_index(package_index)
@@ -46,16 +56,8 @@ def test_load_duplicate_names():
 
 
 def test_load_duplicate_names_semantic():
+    print('Testing duplicate_names_semantic workspace')
     workspaces = [os.path.join(test_data_dir, 'duplicate_names_semantic')]
-    package_index = package_index_from_package_path(workspaces)
-    spec_file_index = spec_file_index_from_package_index(package_index)
-    spec_index, errors = spec_index_from_spec_file_index(spec_file_index)
-    error_types = [type(x) for x in errors]
-    assert discovery.DuplicateNameException in error_types, error_types
-
-
-def test_load_duplicate_names_multi_package():
-    workspaces = [os.path.join(test_data_dir, 'duplicate_names_multi_package')]
     package_index = package_index_from_package_path(workspaces)
     spec_file_index = spec_file_index_from_package_index(package_index)
     spec_index, errors = spec_index_from_spec_file_index(spec_file_index)

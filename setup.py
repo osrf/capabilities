f0237700f89d1536537647a56d3d9051a0ae005d
William Woodall <william@osrfoundation.org>
2013-09-18 13:45:54 -0700

    Install capabilities.specs python subpackage

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['capabilities'],
+    packages=['capabilities', 'capabilities.specs'],
     package_dir={'': 'src'}
 )
 

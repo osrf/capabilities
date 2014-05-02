09fdf7a494e20d40d70e5ff31e2bf474660f7eae
William Woodall <william@osrfoundation.org>
2014-05-02 11:34:41 -0700

    add entry in setup.py to install package data

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['capabilities', 'capabilities.specs'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    package_data={
+        'capabilities': [
+            'capability_server_nodelet_manager.launch',
+            'placeholder_script',
+        ]
+    }
 )
 
 setup(**d)
